9466bc4a1d9795e725b4afd5efaf6ce91a680b0d
vartuom <v.artuom@ya.ru>
2022-01-10 01:24:28 +0800

    js init

---
+++ script.js
@@ -0,0 +1,45 @@
+const content = document.querySelector('.content');
+const cardsContainer = content.querySelector('.cards__list');
+
+const cardSamples = [
+  {
+    header: 'Карачаевск',
+    img: './images/card-karachaevsk.png',
+    desc: 'Фото старого здания на фоне гор'
+  },
+  {
+    header: 'Гора Эльбрус',
+    img: './images/card-elbrus.png',
+    desc: 'Фото горной вершины за линией горизонта'
+  },
+  {
+    header: 'Домбай',
+    img: './images/card-dombai.png',
+    desc: 'Фото горной вершины в облаках'
+  },
+  {
+    header: 'Карачаево-Черкессия',
+    img: './images/card-karachaevsk.png',
+    desc: 'Фото старого здания на фоне гор'
+  }
+];
+
+function generateCard() {
+  const cardTemplate = document.querySelector('#card-template').content;
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const randomCardIndex = Math.floor(Math.random()*cardSamples.length);
+
+  cardElement.querySelector('.card__caption').textContent = cardSamples[randomCardIndex].header;
+  cardElement.querySelector('.card__photo').src = cardSamples[randomCardIndex].img;
+  cardElement.querySelector('.card__photo').alt = cardSamples[randomCardIndex].desc;
+
+  cardsContainer.append(cardElement);
+}
+
+function fillCardsContainer(numCards = 6) {
+  for (let i = 0; i < numCards; i++) {
+    generateCard()
+  }
+}
+
+fillCardsContainer();
